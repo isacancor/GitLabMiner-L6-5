325ef697e421f4e0bd8b02d57513b194924224ed
DelfinSR <delsanrub@alum.us.es>
2023-05-04 17:16:02 +0200

    GitLabMiner is connected to GitMiner

--- src/main/java/aiss/gitlabminer/controller/GitLabController.java
+++ src/main/java/aiss/gitlabminer/controller/GitLabController.java
@@ -1,9 +1,12 @@
 package aiss.gitlabminer.controller;
 
 import aiss.gitlabminer.model.Project;
-import aiss.gitlabminer.repository.ProjectRepository;
+import aiss.gitlabminer.service.GitLabService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -12,15 +15,22 @@ import java.util.List;
 public class GitLabController {
 
     @Autowired
-    ProjectRepository repository;
+    GitLabService service;
+
+    @Autowired
+    RestTemplate restTemplate;
 
     // GET /gitlabminer/{id}[?sinceCommits=5&sinceIssues=30&maxPages=2]
-    @GetMapping("/{id}/")
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/{id}/")
     public Project getProject(@PathVariable String id,
                               @RequestParam int sinceCommits, @RequestParam int sinceIssues,
                               @RequestParam int maxPages) {
-        Project res = repository.getProject(id, sinceCommits, sinceIssues,maxPages);
-        return res;
+        Project res = service.genProject(id, sinceCommits, sinceIssues,maxPages);
+
+        ResponseEntity<Project> response = restTemplate.postForEntity("http://localhost:8080/gitminer/projects",res,Project.class);
+
+        return response.getBody();
     }
 
 }
